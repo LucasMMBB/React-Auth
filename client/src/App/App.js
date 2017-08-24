b888ff7d1a123f3ae16b70d838e349cf18d4cf7b
LucasMMBB <maoxummbb@gmail.com>
2017-08-24 13:37:21 -0400

    finished auth server and connect it to mlab

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -5,14 +5,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <div>
+          <h2>Welcome to React Authentication</h2>
+          <p>You are logged in now</p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
